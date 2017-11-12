74438e48287f6167ff8ff561516c26046ad4aafd
Molex <simplesleeptimer@gmail.com>
2017-11-12 16:19:26 +0100

    Refactor

--- Client/StickClient/__main__.py
+++ Client/StickClient/__main__.py
@@ -1,23 +1,7 @@
-import sys
-import requests
-import socket
-import hashlib
 import json
 import argparse
-from random_words import RandomWords, RandomNicknames
 
-
-def get_ip():
-    """
-    Get the local IP
-    Thanks stackoverflow
-    https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib/25850698#25850698
-    :return:
-    """
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
-    return s.getsockname()[0]
-
+from .ipExchange import IpExchange
 
 if __name__ == '__main__':
     # Read config and parse args
@@ -29,36 +13,24 @@ if __name__ == '__main__':
     url = f"http://{ip}:{port}/transfer"
 
     parser = argparse.ArgumentParser(description="Transfer files over lan")
-    parser.add_argument('-u', '--upload', help='Set mode to upload', action='store_true', default=False)
+    parser.add_argument('-p', '--passphrase', type=str, default=None,
+                        help='Pass phrase for downloading')
+    parser.add_argument('-u', '--upload', help='Set mode to upload',
+                        action='store_true', default=False)
     args = parser.parse_args()
 
+    ip_exchange = IpExchange(config)
+
     if args.upload:
-        # Get connection info
-        ip = get_ip()
-        port = config['listening_port']
-
-        # Generate pass phrase
-        rw = RandomWords()
-        rn = RandomNicknames()
-        passphrase = f"{rn.random_nick(gender='u').capitalize()}{rw.random_word().capitalize()}"
-        # Hash it
-        pass_hash = hashlib.md5(passphrase.encode()).hexdigest()
-        print(f"ip: {ip}\nport: {port}\npass phrase: {passphrase}\nhash: {pass_hash}")
-
-        # Send it off to the server
-        payload = {"id": pass_hash,
-                   "ip": ip,
-                   "port": port}
-        res = requests.post(url, data=payload)
-        print(f"Code:{res.status_code}\n{res.content}")
+        passphrase = ip_exchange.send_info()
+        print(passphrase)
 
     elif not args.upload:
         # Request pass phrase and hash it
-        passphrase = input('Pass phrase: ')
-        pass_hash = hashlib.md5(passphrase.encode()).hexdigest()
-        print(f'Hashed pass phrase: {pass_hash}')
+        if not args.passphrase:
+            passphrase = input('Pass phrase: ')
+        else:
+            passphrase = args.passphrase
 
-        # Request ip and port from server
-        payload = {"id": pass_hash}
-        res = requests.get(url, data=payload)
-        print(f"Code:{res.status_code}\n{res.content.decode()}")
+        con_info = ip_exchange.get_info(passphrase)
+        print(con_info)
---
+++ Client/StickClient/ipExchange.py
@@ -0,0 +1,54 @@
+from random_words import RandomWords, RandomNicknames
+import requests
+import socket
+import hashlib
+import json
+
+
+class IpExchange:
+    def __init__(self, config):
+        self.config = config
+        self.sever_url = f'http://{config["server_ip"]}:{config["server_port"]}'
+
+        self.rw = RandomWords()
+        self.rn = RandomNicknames()
+
+    def send_info(self):
+        # Get connection info
+        ip = self.get_local_ip()
+
+        # Generate pass phrase
+        passphrase = f"{self.rn.random_nick(gender='u').capitalize()}{self.rw.random_word().capitalize()}"
+        # Hash it
+        pass_hash = hashlib.md5(passphrase.encode()).hexdigest()
+
+        # Send it off to the server
+        payload = {"id": pass_hash,
+                   "ip": ip,
+                   "port": self.config["listening_port"]}
+        res = requests.post(f'{self.sever_url}/transfer', data=payload)
+        res.raise_for_status()
+        return passphrase
+
+    def get_info(self, passphrase):
+        # Request pass phrase and hash it
+        pass_hash = hashlib.md5(passphrase.encode()).hexdigest()
+
+        # Request ip and port from server
+        payload = {"id": pass_hash}
+        res = requests.get(f'{self.sever_url}/transfer', data=payload)
+        res.raise_for_status()
+        return json.loads(res.content.decode())
+
+    @staticmethod
+    def get_local_ip():
+        """
+        Get the local IP
+        Thanks stackoverflow
+        https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib/25850698#25850698
+        :return:
+        """
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+        return s.getsockname()[0]
+
