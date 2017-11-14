cecf073b8a00fff5ed649290a73e2238b6dbe83b
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2017-11-14 21:47:40 +0100

    Implement initial file transfer concept

--- Client/StickClient/__main__.py
+++ Client/StickClient/__main__.py
@@ -1,7 +1,9 @@
 import json
 import argparse
+import sys
 
 from .ipExchange import IpExchange
+from .file_transfer import FileTransfer
 
 if __name__ == '__main__':
     # Read config and parse args
@@ -13,13 +15,21 @@ if __name__ == '__main__':
                         help='Pass phrase for downloading')
     parser.add_argument('-u', '--upload', help='Set mode to upload',
                         action='store_true', default=False)
+    parser.add_argument('-t', '--target', type=str, default=None,
+                        help='Target to transmit')
     args = parser.parse_args()
 
     ip_exchange = IpExchange(config)
+    file_transfer = FileTransfer(config)
 
     if args.upload:
+        if not args.target:
+            sys.exit('No upload target provided, you must choose what to send')
+
         passphrase = ip_exchange.send_info()
         print(passphrase)
+        print('Sending file...')
+        file_transfer.send_file('test.txt')
 
     elif not args.upload:
         # Request pass phrase and hash it
@@ -30,3 +40,7 @@ if __name__ == '__main__':
 
         con_info = ip_exchange.get_info(passphrase)
         print(con_info)
+        print('Receiving file')
+        file_transfer.receive_file(con_info['ip'], con_info['port'],
+                                   'test2.txt')
+
--- Client/StickClient/file_transfer.py
+++ Client/StickClient/file_transfer.py
@@ -1,10 +1,39 @@
+import socket
+
 
 class FileTransfer:
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
+        self.buffsize = 4063
 
     def send_file(self, filepath):
-        pass
+        # Establish connection
+        print('Creating socket.')
+        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listen_socket.bind(('0.0.0.0', int(self.config['listening_port']),))
 
-    def receive_file(self):
-        pass
+        print('Awaiting connection')
+        listen_socket.listen()
+        connection, address = listen_socket.accept()
+
+        print('Sending file')
+        with open(filepath, 'rb') as _file:
+            connection.sendfile(_file)
+        print('Done! Closing connection')
+        connection.close()
+
+    def receive_file(self, host, port, filename):
+        # Establish connection
+        print('Creating connection')
+        sending_connection = socket.create_connection((host, port))
+
+        print('Receiving file')
+        # Receiving file
+        with open(filename, 'wb') as _file:
+            while True:
+                data = sending_connection.recv(self.buffsize)
+                if not data:
+                    break
+                _file.write(data)
+        print('Done! Closing connection')
+        sending_connection.close()
